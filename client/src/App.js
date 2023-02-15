10beb89fcca9edf1bdf7c570b046f4aa0672cb10
Jean-Paul <jpzambrano.leon@gmail.com>
2023-02-15 16:48:54 -0600

    modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import "./App.css";
 
 function App() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <ThemeProvider theme={mdTheme}>
+        <Box
+          sx={{
+            display: "flex",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <CssBaseline />
+          {currentUser ? (
+            <Router>
+              <MuiDrawer />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/profile/:username/:page" element={<Profile />} />
+                <Route path="/account" element={<Account />} />
+                <Route path="/assistant/create" element={<CreateAssistant />} />
+                <Route
+                  path="/assistant/:assistantId/chat"
+                  element={<Chats />}
+                />
+              </Routes>
+            </Router>
+          ) : (
+            <Router>
+              <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route
+                  path="/send/verification/:email"
+                  element={<SendEmail />}
+                />
+                <Route
+                  path="/verify/email/:username/:token"
+                  element={<VerifyEmail />}
+                />
+              </Routes>
+            </Router>
+          )}
+        </Box>
+      </ThemeProvider>
     </div>
   );
 }
